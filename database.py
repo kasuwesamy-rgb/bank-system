0caf6734d1cd3133f13b8418fd8d35e1b16b460e
Samy Kasuwe <kasuwesamy@gmail.com>
2026-08-23 08:26:13 -0700

    Configure MySQL for Render

--- database.py
+++ database.py
@@ -3,11 +3,10 @@ import mysql.connector
 
 
 def connect_db():
-
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", ""),
-        database=os.getenv("DB_NAME", "bank_system"),
-        port=int(os.getenv("DB_PORT", "3306"))
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
