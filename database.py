970fbac73930a178c686696d4d04977f9e24eea6
Samy Kasuwe <kasuwesamy@gmail.com>
2026-08-23 07:02:57 -0700

    Preparation database for Render

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
+import os
 import mysql.connector
 
+
 def connect_db():
+
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="bank_system"
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        database=os.getenv("DB_NAME", "bank_system"),
+        port=int(os.getenv("DB_PORT", "3306"))
     )
